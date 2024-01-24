57dbf7cbf26b1785e49f36daecdacbb1af5c1f23
ParkJonghoon0412 <jonghoon.park@g.softbank.co.jp>
2024-01-24 18:04:38 +0900

    first commit

---
+++ contracts/PalToken.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract PalToken is ERC20, ERC20Pausable, Ownable, ERC20Permit {
+    constructor(address initialOwner)
+        ERC20("PalToken", "PTK")
+        Ownable(initialOwner)
+        ERC20Permit("PalToken")
+    {
+        _mint(msg.sender, 1000*10**decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable)
+    {
+        super._update(from, to, value);
+    }
+}
