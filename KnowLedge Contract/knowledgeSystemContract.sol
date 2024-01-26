16afd58503d364f8a1ef86f24b2ca863d4189532
KATORI1010 <hirotoal10100819@gmail.com>
2024-01-26 19:18:09 +0900

    Interface部分の実装見直し

--- KnowLedge Contract/knowledgeSystemContract.sol
+++ KnowLedge Contract/knowledgeSystemContract.sol
@@ -1,24 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// contract IKnowledgeNFT {
-//     function createKnowlegde(string memory knowledge) public {}
-// }
+// Interface for the Contract of Knowledge NFT
+interface IKnowledgeNFT {
+    function createKnowlegde(string memory knowledge) external;
+}
 
-contract IKnowledgeToken {
-    function reward(address to, uint256 amount) public {}
+// Interface for the Contract of Skill Token
+interface IKnowledgeToken {
+    function decimals() external view returns (uint256);
+    function reward(address to, uint256 amount) external;
 }
 
 contract KnowledgeSystem {
     // address IKnowledgeNFTAddress = 0xDc7CF4AF26E512E9E55461F7da527c91406c3f8A;
     // IKnowledgeNFT nftContract = IKnowledgeNFT(IKnowledgeNFTAddress)
 
-    address IKnowledgeTokenAddress = 0x641331167df458511f03D8ceFb4A9697a8a96EdB;
-    IKnowledgeToken tokenContract = IKnowledgeToken(IKnowledgeTokenAddress);
+    // address IKnowledgeTokenAddress = 0x641331167df458511f03D8ceFb4A9697a8a96EdB;
+    // IKnowledgeToken tokenContract = IKnowledgeToken(IKnowledgeTokenAddress);
 
-    uint256 amount = 20 * 10 ** 18;
+    IKnowledgeNFT private nftContract;
+    IKnowledgeToken private tokenContract;
+
+    constructor(address nftContractAddress, address tokenContractAddress) {
+        nftContract = IKnowledgeNFT(nftContractAddress);
+        tokenContract = IKnowledgeToken(tokenContractAddress);
+    }
+
+    uint256 amount = 20 * 10 ** tokenContract.decimals();
     function createKnowledge(string memory knowledge) public {
-        // nftContract.createKnowlegde(knowledge);
+        nftContract.createKnowlegde(knowledge);
         tokenContract.reward(msg.sender, amount);
     }
 }
