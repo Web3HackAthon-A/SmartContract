b6be31d21c5a8322c69738dac24ebcc8986f9cae
KATORI1010 <hirotoal10100819@gmail.com>
2024-01-29 00:28:47 +0900

    スマートコントラクトの改修とフォルダ構成の変更

--- KnowLedge Contract/knowledgeContract.sol
+++ KnowLedge Contract/knowledgeContract.sol
@@ -2,19 +2,22 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract KnowledgeNFT is ERC721 {
+contract KnowledgeNFT is ERC721, AccessControl {
     uint256 public tokenCounter;
     mapping (uint256 => string) public knowledgeData;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC721("KnowledgeNFT", "KNFT") {
+    constructor(address defaultAdmin, address minter) ERC721("KnowledgeNFT", "KNFT") {
         tokenCounter = 0;
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, minter);
     }
 
-    function createKnowlegde(string memory knowledge) public {
+    function createKnowlegde(address knowledgeOwner, string memory knowledge) public onlyRole(MINTER_ROLE) {
         knowledgeData[tokenCounter] = knowledge;
-        _mint(msg.sender, tokenCounter);
+        _mint(knowledgeOwner, tokenCounter);
         tokenCounter++;
     }
 
@@ -24,5 +27,15 @@ contract KnowledgeNFT is ERC721 {
 
     function _baseURI() internal pure override returns (string memory) {
         return "https://raw.githubusercontent.com/Web3HackAthon-A/SmartContract/katori-dev/NFT%20Folder/";
+    }
+
+    // The following functions are overrides required by Solidity.
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
--- KnowLedge Contract/knowledgeSystemContract.sol
+++ KnowLedge Contract/knowledgeSystemContract.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.20;
 
 // Interface for the Contract of Knowledge NFT
 interface IKnowledgeNFT {
-    function createKnowlegde(string memory knowledge) external;
+    function createKnowlegde(address knowledgeOwner, string memory knowledge) external;
+    function getKnowledge(uint256 tokenId) external view returns (string memory);
+    function ownerOf(uint256 tokenId) external view returns (address);
 }
 
 // Interface for the Contract of Skill Token
@@ -14,7 +16,7 @@ interface IKnowledgeToken {
 
 contract KnowledgeSystem {
     // address IKnowledgeNFTAddress = 0xDc7CF4AF26E512E9E55461F7da527c91406c3f8A;
-    // IKnowledgeNFT nftContract = IKnowledgeNFT(IKnowledgeNFTAddress)
+    // IKnowledgeNFT nftContract = IKnowledgeNFT(IKnowledgeNFTAddress);
 
     // address IKnowledgeTokenAddress = 0x641331167df458511f03D8ceFb4A9697a8a96EdB;
     // IKnowledgeToken tokenContract = IKnowledgeToken(IKnowledgeTokenAddress);
@@ -29,7 +31,26 @@ contract KnowledgeSystem {
 
     function createKnowledge(string memory knowledge) public {
         uint256 amount = 20 * 10 ** tokenContract.decimals();
-        nftContract.createKnowlegde(knowledge);
+        nftContract.createKnowlegde(msg.sender, knowledge);
         tokenContract.reward(msg.sender, amount);
     }
+
+    function getKnowledge(uint256 tokenId) public view returns (string memory) {
+        return nftContract.getKnowledge(tokenId);
+    }
+
+    function getKnowledgeOwner(uint256 tokenId) public view returns (address) {
+        return nftContract.ownerOf(tokenId);
+    }
+
+    function rewardAtViewing(uint256 tokenId) public {
+        tokenContract.reward(msg.sender, 5 * 10 ** tokenContract.decimals());
+        rewardForKnowledge(tokenId, 10);
+    }
+
+    function rewardForKnowledge(uint256 tokenId, uint256 amount) public {
+        address ownerAddress = nftContract.ownerOf(tokenId);
+        tokenContract.reward(ownerAddress, amount * 10 ** tokenContract.decimals());
+    }
+
 }
